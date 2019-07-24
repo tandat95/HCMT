34502044b07ab1dd7fc84123174c6adb797ba592
tandat <datca77@gmail.com>
2019-07-24 16:58:38 +0700

    bug

--- Website/Models/IdentityModels.cs
+++ Website/Models/IdentityModels.cs
@@ -9,6 +9,8 @@ namespace HCMT.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public virtual string UserId { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser, string> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
